aa4d7cce1aae6db4c76562b1dc0361652520bc79
5late <christianrfernandes5@gmail.com>
2021-07-11 13:04:50 -0400

    updated infect population

--- disease/disease.py
+++ disease/disease.py
@@ -4,7 +4,8 @@ import asyncio
 CHANCE_OF_INFECTION = 34
 MIN_NUMBER_OF_PEOPLE_MET_DAILY = 0
 MAX_NUMBER_OF_PEOPLE_MET_DAILY = 15
-POPULATION = 10
+POPULATION = 100
+DAYS = 7
 
 async def Infect():
     number_of_people_met = random.randint(MIN_NUMBER_OF_PEOPLE_MET_DAILY, MAX_NUMBER_OF_PEOPLE_MET_DAILY)
@@ -13,27 +14,33 @@ async def Infect():
             calculate_infection = random.randint(0, 1000)
             if calculate_infection < CHANCE_OF_INFECTION:
                 is_infected = True
-                return is_infected
+                return is_infected, number_of_people_met
             else:
                 is_infected = False
-        return is_infected
+        return is_infected, number_of_people_met
     else:
         is_infected = False
-        return is_infected
+        return is_infected, number_of_people_met
 
 async def startSimulation():
-    global infected
-    infected = 0
-    for i in range(POPULATION):
-        is_infected = await Infect()
-        if is_infected:
-            infected += 1
+    global infected_population
+    infected_population = 0
+    healthy_population = POPULATION
+    for i in range(DAYS):
+        for i in range(healthy_population):
+            is_infected, number_of_people_met = await Infect()
+            if is_infected:
+                infected_population += 1
+                healthy_population -= 1
+            print(f'Number of people met: {number_of_people_met}, Infected: {is_infected}')
+        await asyncio.sleep(1)
+        print('---1 day passed---')
 
 def main():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(startSimulation())
     #print('YEARS PASSED: ' + str(years))
-    print('Number of Infections: ' + str(infected))
+    print('Number of Infections: ' + str(infected_population))
 
 main()
