b678ea45e7d1093e4918bc42c4b434d383957e3a
5late <christianrfernandes5@gmail.com>
2021-07-10 20:42:11 -0400

    only can breed if over 20 years, 1-3 kids

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import asyncio
 
-GENDERS = ['male', 'female']
-MIN_NUMBER_OF_CHILDREN = 0
+SEXES = ['male', 'female']
+MIN_NUMBER_OF_CHILDREN = 1
 MAX_NUMBER_OF_CHILDREN = 3
 
 async def Breed():
@@ -11,8 +11,7 @@ async def Breed():
     males = 0
     females = 0
     while current_child < children_produced:
-        child_sex = random.choice(GENDERS)
-        print(child_sex)
+        child_sex = random.choice(SEXES)
         if child_sex == 'male':
             males = males + 1
         else:
@@ -21,44 +20,77 @@ async def Breed():
     
     return children_produced, males, females
     
+  
 async def startGame():
-    population = 0
+    global years
+    years = 0
+    
+    population = 3
     males = 2
     females = 1
+    available_males = 2
+    available_females = 1
+    cache_females = []
+    cache_males = []
 
-    while population < 1000000:
-        if males > females:
-            current_sex = 0
-            if current_sex < females:
-                number_of_children_produced, boys, girls = await Breed()
-                males += boys
-                females += girls
-                population += number_of_children_produced
-                print(population, number_of_children_produced)
-        elif females > males:
-            current_sex = 0
-            if current_sex < males:
-                number_of_children_produced, boys, girls = await Breed()
-                males += boys
-                females += girls
-                population += number_of_children_produced
-                print(population, number_of_children_produced)
-        else:
-            current_sex = 0
-            if current_sex < males:
-                number_of_children_produced, boys, girls = await Breed()
-                males += boys
-                females += girls
-                population += number_of_children_produced
-                print(population, number_of_children_produced)
-
-        print(males, females)
-        await asyncio.sleep(1)
-        print('---1 year passed---')
-
+    while population < 10000:
+        if years >= 20:
+            if len(cache_males) > 20:
+                available_females += cache_females[0]
+                cache_males.pop(0)
+            if len(cache_females) > 20:
+                available_males += cache_males[0]
+                cache_females.pop(0)
+            if available_males > available_females:
+                current_sex = 0
+                while current_sex < available_females:
+                    number_of_children_produced, boys, girls = await Breed()
+                    males += boys
+                    females += girls
+                    population += number_of_children_produced
+                    cache_males.append(boys)
+                    cache_females.append(girls)
+                    print(number_of_children_produced)
+                    current_sex += 1
+            elif available_females > available_males:
+                current_sex = 0
+                while current_sex < available_males:
+                    number_of_children_produced, boys, girls = await Breed()
+                    males += boys
+                    females += girls
+                    population += number_of_children_produced
+                    cache_males.append(boys)
+                    cache_females.append(girls)
+                    print(number_of_children_produced)
+                    current_sex += 1
+            else:
+                current_sex = 0
+                while current_sex < available_males:
+                    number_of_children_produced, boys, girls = await Breed()
+                    males += boys
+                    females += girls
+                    population += number_of_children_produced
+                    cache_males.append(boys)
+                    cache_females.append(girls)
+                    print(number_of_children_produced)
+                    current_sex += 1
+            
+            print(f"Males: {males}",f"Females: {females}, Total Population: {population}")
+            print(f'Available males: {available_males}, Available females: {available_females}')
+            await asyncio.sleep(.5)
+            years += 1
+            print('---1 year passed---')
+        if years < 20:
+            print(f"Males: {males}",f"Females: {females}, Total Population: {population}")
+            await asyncio.sleep(.5)
+            years += 1
+            print('---1 year passed---')        
+            
+    
 def main():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(startGame())
+    print('YEARS PASSED: ' + str(years))
 
 main()
